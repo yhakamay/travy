b42fd869e9227956c857716a4a5b2ca135a78a3f
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-05 23:50:01 +0900

    remove GitHub link from Header

--- src/app/components/organisms/header.tsx
+++ src/app/components/organisms/header.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BsGithub } from "react-icons/bs";
-import {
-  Box,
-  Container,
-  Heading,
-  HStack,
-  IconButton,
-} from "../atoms/my_chakra_components";
+import { Box, Container, Heading, HStack } from "../atoms/my_chakra_components";
 
 export default function Header() {
   return (
@@ -20,17 +13,6 @@ export default function Header() {
                 <Image src="/logo.png" width={32} height={32} alt={"logo"} />
                 <Heading size="lg">Travy</Heading>
               </HStack>
-            </Link>
-            <Link
-              href={"https://github.com/yhakamay/travy"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconButton
-                variant="link"
-                icon={<BsGithub />}
-                aria-label={"github"}
-              />
             </Link>
           </HStack>
         </Container>
